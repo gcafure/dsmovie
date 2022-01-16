593c134d8425b126a7b9eb4a007e34a2ed6070c8
Thrysk <guicafure@gmail.com>
2022-01-16 16:50:31 -0400

    show score

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import MovieCard from "components/MovieCard";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
