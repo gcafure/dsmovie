eae2f4361fba2426ef4ee36bdff6326541550a86
Thrysk <guicafure@gmail.com>
2022-01-16 20:54:09 -0400

    changing homepage link to react-router-dom link

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,24 +1,26 @@
-import { ReactComponent as GithubIcon } from 'assets/img/Github.svg';
-import './styles.css';
+import { ReactComponent as GithubIcon } from "assets/img/Github.svg";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
-function Navbar(){
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>
+            <Link to="/">DSMovie</Link>
+          </h1>
 
-    return (
-        <header>
-        <nav className="container">
-            <div className="dsmovie-nav-content">
-                <h1><a href = "/"> DSMovie</a></h1>
-                <a href="https://github.com/thrysk" target="_blank" rel="noreferrer">
-                    <div className="dsmovie-contact-container">
-                        <GithubIcon />
-                        <p className="dsmovie-contact-link">/thrysk</p>
-                    </div>
-                </a>
+          <a href="https://github.com/thrysk" target="_blank" rel="noreferrer">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/thrysk</p>
             </div>
-        </nav>
+          </a>
+        </div>
+      </nav>
     </header>
-    );
-
+  );
 }
 
 export default Navbar;
